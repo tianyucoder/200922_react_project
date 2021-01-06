d3587ac3e380ea5c0e4e0ff02c5d1862ae882d87
tianyu5177 <zhangtianyu5177@sina.com>
2021-01-06 16:18:42 +0800

    更新

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+const { override, fixBabelImports,addLessLoader } = require('customize-cra');
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd', //对哪个库进行按需引入
+    libraryDirectory: 'es', //要按需引入的这个库，使用的是哪种模块化规范
+    style: true, //对哪种文件进行按需引入
+	}),
+	addLessLoader({
+		lessOptions:{
+			javascriptEnabled: true, //允许js修改antd的less文件
+			modifyVars: { '@primary-color': 'green' },
+		}
+	}),
+);
